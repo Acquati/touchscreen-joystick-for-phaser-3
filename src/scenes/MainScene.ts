cd2d85edf94158d6a711dde8bba7a904c3e6075b
Leandro Acquati <leandroacquati@gmail.com>
2022-01-07 01:17:59 -0300

    add plugin from github

--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -4,28 +4,63 @@ import PhaserLogo from '../objects/PhaserLogo'
 import GameConfig from '../config'
 
 export default class MainScene extends Phaser.Scene {
-  private phaserLogo!: PhaserLogo
+  private joyStick!: any
+  private text!: Phaser.GameObjects.Text
 
   constructor() {
     super({ key: SceneKeys.MainScene })
   }
 
+  preload() {
+    const rexVirtualJoystickPluginUrl =
+      'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexvirtualjoystickplugin.min.js'
+    this.load.plugin(
+      'rexvirtualjoystickplugin',
+      rexVirtualJoystickPluginUrl,
+      true
+    )
+  }
+
   create() {
-    const particles = this.add.particles(TextureKeys.RedParticle)
+    this.joyStick = this.plugins
+      .get('rexvirtualjoystickplugin')
+      .add(this, {
+        x: 400,
+        y: 300,
+        radius: 70,
+        base: this.add.circle(0, 0, 70, 0x888888, 0.2),
+        thumb: this.add.circle(0, 0, 30, 0xcccccc, 0.2),
+        // dir: '8dir',   // 'up&down'|0|'left&right'|1|'4dir'|2|'8dir'|3
+        forceMin: 10
+        // enable: true
+      })
+      .on('update', this.dumpJoyStickState, this)
 
-    const emitter = particles.createEmitter({
-      speed: 100,
-      scale: { start: 0.5, end: 0 },
-      blendMode: 'ADD'
-    })
+    console.log(this.joyStick)
 
-    this.phaserLogo = new PhaserLogo({
-      scene: this,
-      x: Math.floor(Number(GameConfig.width) / 2),
-      y: Math.floor(Number(GameConfig.height) / 2),
-      texture: TextureKeys.PhaserLogo
-    })
+    this.text = this.add.text(0, 0, '')
+    this.dumpJoyStickState()
+  }
 
-    emitter.startFollow(this.phaserLogo)
+  private dumpJoyStickState() {
+    const cursorKeys = this.joyStick.createCursorKeys()
+    var joystickValues = 'Key down: '
+    for (let name in cursorKeys) {
+      if (cursorKeys[name].isDown) {
+        joystickValues += `${name} `
+      }
+    }
+
+    joystickValues += `
+Force: ${Math.floor(this.joyStick.force * 100) / 100}
+Angle: ${Math.floor(this.joyStick.angle * 100) / 100}
+`
+
+    joystickValues += '\nTimestamp:\n'
+    for (let name in cursorKeys) {
+      const key = cursorKeys[name]
+      joystickValues += `${name}: duration=${key.duration / 1000}\n`
+    }
+    this.text.setText(joystickValues)
   }
 }
--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -6,11 +6,6 @@ export default class Preloader extends Phaser.Scene {
     super({ key: SceneKeys.Preloader })
   }
 
-  preload() {
-    this.load.image(TextureKeys.PhaserLogo, 'images/phaser3-logo.png')
-    this.load.image(TextureKeys.RedParticle, 'images/red.png')
-  }
-
   create() {
     this.scene.start(SceneKeys.MainScene)
   }
